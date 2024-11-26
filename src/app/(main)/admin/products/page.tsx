476bb7aa548880f413d83e0d3936431a93a3f313
NAMND <namndph37181@fpt.edu.vn>
2024-11-26 21:21:41 +0700

    fix fill data products

--- src/app/(main)/admin/products/page.tsx
+++ src/app/(main)/admin/products/page.tsx
@@ -1,11 +1,33 @@
+'use client'
+import { useEffect, useState } from 'react'
 import ProductService from '@/service/ProducrService'
 import ProductList from './details/_components/ProductList'
+import { ProductResponse } from '@/interface/Product'
+import Spinner from '@/components/spinner/Spinner'
 
-export default async function ProductPage() {
-    const products = await ProductService.getAllProducts()
+export default function ProductPage() {
+    const [products, setProducts] = useState<ProductResponse[]>([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        async function fetchProducts() {
+            setIsLoading(true)
+            try {
+                const data = await ProductService.getAllProducts()
+                setProducts(data)
+            } catch (error) {
+                console.error('Failed to fetch products:', error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetchProducts()
+    }, [])
 
     return (
         <>
+            <Spinner isLoading={isLoading} />
             <ProductList products={products} />
         </>
     )
