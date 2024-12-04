4e078c43b9e2bae5aa409ed721bb7bef3d37d6d1
✨khanh🌟 <hoangkhanhvn179@gmail.com>
2024-12-05 03:44:27 +0700

    update return amount for customer

--- src/app/(main)/admin/retail/_components/PaymentDialog.tsx
+++ src/app/(main)/admin/retail/_components/PaymentDialog.tsx
@@ -1,12 +1,9 @@
 import { Button } from 'primereact/button'
-import { Column } from 'primereact/column'
-import { DataTable } from 'primereact/datatable'
 import { Dialog } from 'primereact/dialog'
-import { InputNumber, InputNumberValueChangeEvent } from 'primereact/inputnumber'
+import { InputNumber } from 'primereact/inputnumber'
 import { Toast } from 'primereact/toast'
 import { ToggleButton } from 'primereact/togglebutton'
-import { useRef, useState } from 'react'
-
+import { useMemo, useRef, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useLocalStorage } from 'primereact/hooks'
 
@@ -18,25 +15,15 @@ type PaymentDialogProps = {
     amountPaid: number
 }
 
-export default function PaymentDialog({
-    visible,
-    setVisible,
-    totalAmount,
-    setAmountPaid,
-    amountPaid
-}: PaymentDialogProps) {
+export default function PaymentDialog({ visible, setVisible, totalAmount, setAmountPaid }: PaymentDialogProps) {
     const [checked, setChecked] = useState(true)
     const toast = useRef<Toast>(null)
     const [amountPaidState, setAmountPaidState] = useState(0)
     const router = useRouter()
     const [, setAmountPaidLocal] = useLocalStorage<number>(0, 'amountPaid')
-    const onSetAmountPaid = (e: InputNumberValueChangeEvent) => {
-        setAmountPaidState(e.value ?? 0)
-    }
 
     const onHide = () => {
         setVisible(false)
-
         setAmountPaidState(0)
     }
 
@@ -50,7 +37,7 @@ export default function PaymentDialog({
             })
             return
         }
-        setAmountPaid(amountPaidState)
+        setAmountPaid(totalAmount)
         toast.current?.show({
             severity: 'success',
             summary: 'Success',
@@ -67,6 +54,9 @@ export default function PaymentDialog({
         setAmountPaidLocal(totalAmount)
         router.push('/admin/checkout')
     }
+
+    const amountRemaining = useMemo(() => Math.max(0, totalAmount - amountPaidState), [totalAmount, amountPaidState])
+    const amountExcess = useMemo(() => Math.max(0, amountPaidState - totalAmount), [totalAmount, amountPaidState])
 
     return (
         <Dialog
@@ -87,7 +77,7 @@ export default function PaymentDialog({
                     <ToggleButton
                         checked={checked}
                         onChange={(e) => setChecked(e.value)}
-                        className='text-xl font-medium text-black '
+                        className='text-xl font-medium text-black'
                         onLabel='Cash'
                         offLabel='Cash'
                         style={{ width: '100%' }}
@@ -95,13 +85,13 @@ export default function PaymentDialog({
                     <ToggleButton
                         checked={!checked}
                         onChange={onTransfer}
-                        className='text-xl font-medium text-black '
+                        className='text-xl font-medium text-black'
                         onLabel='Transfer'
                         offLabel='Transfer'
                         style={{ width: '100%' }}
                     />
                 </div>
-                <div className='flex flex-col gap-1 '>
+                <div className='flex flex-col gap-1'>
                     <label className='text-xl ms-0 font-medium text-gray-900 dark:text-white'>
                         Amount Customer Paid
                     </label>
@@ -110,27 +100,20 @@ export default function PaymentDialog({
                         className='w-full'
                         value={amountPaidState}
                         min={0}
-                        max={totalAmount}
                         showButtons
-                        onValueChange={onSetAmountPaid}
+                        onChange={(e) => setAmountPaidState(e.value ?? 0)}
                     />
-                </div>
-                <div className='flex items-center gap-2'>
-                    <DataTable value={[]} className='w-full'>
-                        <Column field='#' header='#'></Column>
-                        <Column field='Code' header='Code'></Column>
-                        <Column field='Payment Method' header='Payment Method'></Column>
-                        <Column field='Amount' header='Amount'></Column>
-                        <Column field='Date Paid' header='Date Paid'></Column>
-                    </DataTable>
                 </div>
                 <div className='flex justify-between items-center gap-2'>
                     <label className='text-xl ms-0 font-medium text-gray-900 dark:text-white'>Amount Remaining</label>
-                    <div className='text-xl font-medium text-primary-700 dark:text-white'>
-                        ${amountPaid > totalAmount ? 0 : totalAmount - amountPaid}
-                    </div>
+                    <div className='text-xl font-medium text-primary-700 dark:text-white'>${amountRemaining}</div>
                 </div>
-
+                <div className='flex justify-between items-center gap-2'>
+                    <label className='text-xl ms-0 font-medium text-gray-900 dark:text-white'>
+                        Excess Amount (Change)
+                    </label>
+                    <div className='text-xl font-medium text-green-700 dark:text-white'>${amountExcess}</div>
+                </div>
                 <div className='flex justify-end items-center gap-2'>
                     <Button label='Save' icon='pi pi-save' onClick={onSave} />
                 </div>
