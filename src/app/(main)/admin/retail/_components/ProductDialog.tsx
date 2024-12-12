6737209c34b4cb28d5fc0df7ee7595bd1cabb652
✨khanh🌟 <hoangkhanhvn179@gmail.com>
2024-12-12 16:59:23 +0700

    feat: enhance product filtering with initial filter settings and numeric input for price and quantity

--- src/app/(main)/admin/retail/_components/ProductDialog.tsx
+++ src/app/(main)/admin/retail/_components/ProductDialog.tsx
@@ -7,6 +7,7 @@ import { FilterMatchMode } from 'primereact/api'
 import { FaCartPlus } from 'react-icons/fa'
 import { ProductResponse } from '@/interface/Product'
 import { Image } from 'primereact/image'
+import { useEffect, useMemo } from 'react'
 
 interface ProductDialogProps {
     products: ProductResponse[]
@@ -31,6 +32,19 @@ const ProductDialog = ({
     globalFilterValue,
     onGlobalFilterChange
 }: ProductDialogProps) => {
+    const initialFilters: DataTableFilterMeta = useMemo(
+        () => ({
+            global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+            name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+            sku: { value: null, matchMode: FilterMatchMode.CONTAINS },
+            categoryName: { value: null, matchMode: FilterMatchMode.EQUALS },
+            manufacturerName: { value: null, matchMode: FilterMatchMode.EQUALS },
+            price: { value: null, matchMode: FilterMatchMode.GREATER_THAN_OR_EQUAL_TO },
+            quantity: { value: null, matchMode: FilterMatchMode.GREATER_THAN_OR_EQUAL_TO }
+        }),
+        []
+    )
+
     const renderHeader = () => {
         return (
             <div className='flex justify-content-between'>
@@ -39,7 +53,7 @@ const ProductDialog = ({
                     icon='pi pi-filter-slash'
                     label='Làm Mới'
                     outlined
-                    onClick={() => setFilters({ global: { value: null, matchMode: FilterMatchMode.CONTAINS } })}
+                    onClick={() => setFilters(initialFilters)}
                 />
                 <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder='Tìm kiếm từ khóa' />
             </div>
@@ -51,6 +65,11 @@ const ProductDialog = ({
     const indexBodyTemplate = (_: ProductResponse, options: { rowIndex: number }) => {
         return <>{options.rowIndex + 1}</>
     }
+
+    useEffect(() => {
+        setFilters(initialFilters)
+    }, [initialFilters, setFilters])
+
     return (
         <Dialog
             header='Tìm Sản Phẩm'
@@ -67,6 +86,7 @@ const ProductDialog = ({
                 header={renderHeader()}
                 emptyMessage='Không tìm thấy sản phẩm nào.'
                 onFilter={onFilter}
+                filterDisplay='menu'
             >
                 <Column
                     header='#'
@@ -94,13 +114,19 @@ const ProductDialog = ({
                         </div>
                     )}
                 />
-                <Column field='name' header='Tên Sản Phẩm' />
-                <Column field='sku' header='SKU' />
-                <Column field='categoryName' header='Danh Mục' />
-                <Column field='manufacturerName' header='Nhà Sản Xuất' />
+                <Column field='name' header='Tên Sản Phẩm' filter filterPlaceholder='Tìm theo tên' />
+                <Column field='sku' header='SKU' filter filterPlaceholder='Tìm theo SKU' />
+                <Column field='categoryName' header='Danh Mục' filter filterPlaceholder='Tìm theo danh mục' />
+                <Column
+                    field='manufacturerName'
+                    header='Nhà Sản Xuất'
+                    filter
+                    filterPlaceholder='Tìm theo nhà sản xuất'
+                />
                 <Column
                     field='price'
                     header='Giá'
+                    dataType='numeric'
                     body={(rowData: ProductResponse) => (
                         <div className='flex gap-2 items-center'>
                             {rowData.discountPrice ? (
@@ -113,9 +139,38 @@ const ProductDialog = ({
                             )}
                         </div>
                     )}
+                    filter
+                    filterElement={(options) => (
+                        <div className='flex flex-col gap-2'>
+                            <InputText
+                                type='number'
+                                value={options.value}
+                                onChange={(e) => options.filterCallback(parseInt(e.target.value), options.index)}
+                                placeholder='Giá tối thiểu'
+                                className='w-full'
+                                min={0}
+                            />
+                        </div>
+                    )}
                 />
-                <Column field='quantity' header='Số lượng' />
-
+                <Column
+                    field='quantity'
+                    header='Số lượng'
+                    dataType='numeric'
+                    filter
+                    filterElement={(options) => (
+                        <div className='flex flex-col gap-2'>
+                            <InputText
+                                type='number'
+                                value={options.value}
+                                onChange={(e) => options.filterCallback(parseInt(e.target.value), options.index)}
+                                placeholder='Số lượng tối thiểu'
+                                className='w-full'
+                                min={0}
+                            />
+                        </div>
+                    )}
+                />
                 <Column
                     header='Thao Tác'
                     body={(rowData) => (
